9cb2b041dc7e0a692b6d048157625395000b4296
xiubert <patrickcody@pitt.edu>
2025-05-21 15:22:18 -0400

    clean up oddball stim script

--- stimuliGeneration/genOddballStim_hardcodedGain.m
+++ stimuliGeneration/genOddballStim_hardcodedGain.m
@@ -13,7 +13,7 @@
 % Intensity: 80 dB SPL
 
 
-%%
+%% SET PARAMETERS
 signalSavePath = 'C:\DATA\Charlie\sinewavePulseFiles\250kHzPulses\oddball'; %Folder for .signal files
 fSampling = 250000; %sample rate for signal | samples / s | 250kHz is max dictated by the NI-DAQ
 bitDepth = 16; %bit depth of DAQ (USB-6229 is 16-bit)
@@ -21,30 +21,42 @@ dither = true;
 stimOnset = 3; %seconds | time of tone onset in signal
 pulseLen = 100; %ms | duration of pure-tone (just pure-tone not entire signal)
 ISI = 500; %ms
-% nStim = 3;
 afterStim = 4000; %ms
 rampType = 'sinSquared';
 rampTime = 5; %ms
 dB = '80';
+stimLength = 60; %s
+pctDeviantTones = 10;
 
 pulseLen = pulseLen/1000;
 rampTime = rampTime/1000;
 afterStim = afterStim/1000;
 ISI = ISI/1000;
+pctDeviantTones = pctDeviantTones/100;
 
-%% make random stim times
-stimVec = ones(1, 120);
-randloc = randsample(120, 12);
+
+nTones = stimLength./ISI;
+
+%% make random stim sequence
+% define standard and oddball frequencies (usually an octave apart)
+% should exist in calibration data
+freq = [7711 15422];
+
+% standard tone is frequency index 1
+stimVec = ones(1, nTones);
+randloc = randsample(nTones, nTones*pctDeviantTones);
 while min(randloc)<4
-    randloc = randsample(120, 12);
+    randloc = randsample(nTones, nTones*pctDeviantTones);
 end
+
+% oddball is frequency index 2
 stimVec(randloc) = 2;
 randloc
 % 20250519
 % randloc = [12; 86; 82; 57; 61; 21; 9; 63; 8; 6; 118; 59];
 
 
-%% create pure-tones and train matrix
+%% create pure-tones with onset/offset mask
 
 tPulse = 0:1/fSampling:pulseLen-(1/fSampling);
 %create pulse for each frequency
@@ -69,7 +81,8 @@ end
 %scale tones for each frequency by ramp mask
 rampMaskedTones = toneRampMask.*tones;
 
-%% Load calibration file w/ frequencies
+%% Load calibration file for gain needed to achieve desired dB for selected frequencies
+
 [calFile,calFilPath] = uigetfile(...
     'C:\Data\Rig Software\speakerCalibration\calibrationOutput*.mat',...
     'Load inverse filter calibration file for respective frequencies');
@@ -79,8 +92,7 @@ calSname = calSname{1};
 uMicCalV = mean(calS.(calSname).micCalV);
 
 %% get gain values for tones and gain tones
-% ODDBALL IS ALWAYS SECOND
-freq = [7711 15422];
+
 gainf = zeros(1,length(freq));
 
 for f = 1:length(freq)
@@ -90,18 +102,21 @@ end
 
 gainedRampMaskedTones = gainf'.*rampMaskedTones;
 
-%% make stim
+%% make stim sequence with gained tones
+
+% set pre-stim
 addvec = zeros(1,stimOnset*fSampling);
+% add each tone in sequence w/ ISI
 for stimpos = 1:length(stimVec)
     addvec = cat(2,addvec,[gainedRampMaskedTones(stimVec(stimpos),:) zeros(1,((ISI)-(pulseLen))*fSampling)]);
 end
-% add after stim
+% add time after stim
 y = cat(2,addvec,zeros(1,afterStim*fSampling));
 %% look at stim
 tStim = 0:1/fSampling:length(y)/fSampling-(1/fSampling);
 plot(tStim,y)
 
-%% make stim file
+%% make stim SignalObject file for Ephus
 
 %quantize and dither
 y = quantize_dither(y, bitDepth, dither);
